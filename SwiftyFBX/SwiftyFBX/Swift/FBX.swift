5df03785d1ee30d59b77ea363692e603bdc5649f
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2021-01-21 23:23:14 +0900

    Update FBXScene

--- SwiftyFBX/SwiftyFBX/Swift/FBX.swift
+++ SwiftyFBX/SwiftyFBX/Swift/FBX.swift
@@ -9,10 +9,14 @@ import Foundation
 
 public class FBX {
     let version: String
+    let scene: FBXScene?
     let meshs: Array<Any>
-    
-    init() {
+    let textures: Array<Any>
+
+    init(scene: FBXScene?) {
         self.version = "1.0.0"
+        self.scene = scene
         self.meshs = Array()
+        self.textures = Array()
     }
 }

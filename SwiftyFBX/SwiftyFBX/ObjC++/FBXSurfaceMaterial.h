04ca19f0a16604076305da11608eb2e272b893c0
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2021-01-31 00:01:32 +0900

    Add property of FbxSurfaceMaterial to FBXSurfaceMaterial.mm

--- SwiftyFBX/SwiftyFBX/ObjC++/FBXSurfaceMaterial.h
+++ SwiftyFBX/SwiftyFBX/ObjC++/FBXSurfaceMaterial.h
@@ -11,7 +11,52 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FBXSurfaceMaterial : NSObject
 @property (nonatomic, readonly, getter=getName) NSString *name;
+@property (nonatomic, readonly, getter=getShadingModelName) NSString *shadingModelName;
+@property (nonatomic, readonly, getter=getMultiLayerName) NSString *multiLayerName;
+@property (nonatomic, readonly, getter=getEmissiveName) NSString *emissiveName;
+@property (nonatomic, readonly, getter=getEmissiveFactorName) NSString *emissiveFactorName;
+@property (nonatomic, readonly, getter=getAmbientName) NSString *ambientName;
+@property (nonatomic, readonly, getter=getAmbientFactorName) NSString *ambientFactorName;
+@property (nonatomic, readonly, getter=getDiffuseName) NSString *diffuseName;
+@property (nonatomic, readonly, getter=getDiffuseFactorName) NSString *diffuseFactorName;
+@property (nonatomic, readonly, getter=getSpecularName) NSString *specularName;
+@property (nonatomic, readonly, getter=getSpecularFactorName) NSString *specularFactorName;
+@property (nonatomic, readonly, getter=getShininessName) NSString *shininessName;
+@property (nonatomic, readonly, getter=getBumpName) NSString *bumpName;
+@property (nonatomic, readonly, getter=getBumpFactorName) NSString *bumpFactorName;
+@property (nonatomic, readonly, getter=getNormalMapName) NSString *normalMapName;
+@property (nonatomic, readonly, getter=getTransparentColorName) NSString *transparentColorName;
+@property (nonatomic, readonly, getter=getTransparencyFactorName) NSString *transparencyFactorName;
+@property (nonatomic, readonly, getter=getReflectionName) NSString *reflectionName;
+@property (nonatomic, readonly, getter=getReflectionFactorName) NSString *reflectionFactorName;
+@property (nonatomic, readonly, getter=getDisplacementColorName) NSString *displacementColorName;
+@property (nonatomic, readonly, getter=getDisplacementFactorName) NSString *displacementFactorName;
+@property (nonatomic, readonly, getter=getVectorDisplacementColorName) NSString *vectorDisplacementColorName;
+@property (nonatomic, readonly, getter=getVectorDisplacementFactorName) NSString *vectorDisplacementFactorName;
+
 - (NSString *)getName;
+- (NSString *)getShadingModelName;
+- (NSString *)getMultiLayerName;
+- (NSString *)getEmissiveName;
+- (NSString *)getEmissiveFactorName;
+- (NSString *)getAmbientName;
+- (NSString *)getAmbientFactorName;
+- (NSString *)getDiffuseName;
+- (NSString *)getDiffuseFactorName;
+- (NSString *)getSpecularName;
+- (NSString *)getSpecularFactorName;
+- (NSString *)getShininessName;
+- (NSString *)getBumpName;
+- (NSString *)getBumpFactorName;
+- (NSString *)getNormalMapName;
+- (NSString *)getTransparentColorName;
+- (NSString *)getTransparencyFactorName;
+- (NSString *)getReflectionName;
+- (NSString *)getReflectionFactorName;
+- (NSString *)getDisplacementColorName;
+- (NSString *)getDisplacementFactorName;
+- (NSString *)getVectorDisplacementColorName;
+- (NSString *)getVectorDisplacementFactorName;
 @end
 
 NS_ASSUME_NONNULL_END
--- SwiftyFBX/SwiftyFBX/ObjC++/FBXSurfaceMaterial.mm
+++ SwiftyFBX/SwiftyFBX/ObjC++/FBXSurfaceMaterial.mm
@@ -43,4 +43,253 @@
                               encoding:NSUTF8StringEncoding];
 }
 
+- (NSString *)getShadingModelName
+{
+    const char* pValue = _cMaterial->sShadingModel;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    FbxPropertyT<FbxString> str = _cMaterial->ShadingModel;
+        
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getMultiLayerName
+{
+    const char* pValue = _cMaterial->sMultiLayer;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getEmissiveName;
+{
+    const char* pValue = _cMaterial->sEmissive;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getEmissiveFactorName
+{
+    const char* pValue = _cMaterial->sEmissiveFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getAmbientName
+{
+    const char* pValue = _cMaterial->sAmbient;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getAmbientFactorName
+{
+    const char* pValue = _cMaterial->sAmbientFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getDiffuse
+{
+    const char* pValue = _cMaterial->sDiffuse;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getDiffuseFactorName
+{
+    const char* pValue = _cMaterial->sDiffuseFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getSpecularName
+{
+    const char* pValue = _cMaterial->sSpecular;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getSpecularFactorName
+{
+    const char* pValue = _cMaterial->sSpecularFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getShininessName
+{
+    const char* pValue = _cMaterial->sShininess;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getBumpName
+{
+    const char* pValue = _cMaterial->sBump;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getNormalMapName
+{
+    const char* pValue = _cMaterial->sNormalMap;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getBumpFactorName
+{
+    const char* pValue = _cMaterial->sBumpFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getTransparentColorName
+{
+    const char* pValue = _cMaterial->sTransparentColor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getTransparencyFactorName
+{
+    const char* pValue = _cMaterial->sTransparencyFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getReflectionName
+{
+    const char* pValue = _cMaterial->sReflection;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getReflectionFactorName
+{
+    const char* pValue = _cMaterial->sReflectionFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getDisplacementColorName
+{
+    const char* pValue = _cMaterial->sDisplacementColor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getDisplacementFactorName
+{
+    const char* pValue = _cMaterial->sDisplacementFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getVectorDisplacementColorName
+{
+    const char* pValue = _cMaterial->sVectorDisplacementColor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (NSString *)getVectorDisplacementFactorName
+{
+    const char* pValue = _cMaterial->sVectorDisplacementFactor;
+    if (pValue == NULL) {
+        return @"";
+    }
+    
+    return [NSString stringWithCString:pValue
+                              encoding:NSUTF8StringEncoding];
+}
+
+- (BOOL)isPhong
+{
+    return _cMaterial->GetClassId().Is(FbxSurfacePhong::ClassId);
+}
+
 @end
